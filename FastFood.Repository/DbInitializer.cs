25628cd64d6fe86e26160ba026d074c3d901b8f4
Rao K <Rao_K@Dellteam.com>
2024-03-29 17:47:22 +0530

    Added db connection

--- FastFood.Repository/DbInitializer.cs
+++ FastFood.Repository/DbInitializer.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using FastFood.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FastFood.Repository
 {
@@ -7,10 +9,43 @@ namespace FastFood.Repository
 		private readonly RoleManager<IdentityRole> _roleManager;
 		private readonly UserManager<IdentityUser> _identityUser;
 		private readonly ApplicationDBContext _context;
-
+		public DbInitializer(RoleManager<IdentityRole> roleManager,
+			UserManager<IdentityUser> identityUser,
+			ApplicationDBContext context)
+		{
+			_roleManager = roleManager;
+			_identityUser = identityUser;
+			_context = context;
+		}
 		public void Initialize()
 		{
-			throw new NotImplementedException();
+			try
+			{
+				if (_context.Database.GetPendingMigrations().Count() > 0) { 
+				_context.Database.Migrate();
+				}
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+
+			if (_context.Roles.Any(x => x.Name == "Admin")) return;
+			_roleManager.CreateAsync(new IdentityRole("Manager")).GetAwaiter().GetResult();
+			_roleManager.CreateAsync(new IdentityRole("Admin")).GetAwaiter().GetResult();
+			_roleManager.CreateAsync(new IdentityRole("Customer")).GetAwaiter().GetResult();
+
+			var applicationUser = new ApplicationUser() {
+			UserName = "admin@gmail.com",
+			Email ="admin@gmail.com",
+			Name ="Admin",
+			City ="Blore",
+			Address="Blore",
+			PostalCode="12345"
+			};
+
+			_identityUser.CreateAsync(applicationUser, "Admin@123").GetAwaiter().GetResult();
+			_identityUser.AddToRoleAsync(applicationUser, "Admin");
 		}
 	}
 }
